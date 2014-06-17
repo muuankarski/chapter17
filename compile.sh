a1f5132a4b359a609e4442854fbf5c4e110ac42a
Markus Kainu <markuskainu@gmail.com>
2014-06-17 10:24:39 +0300

    article updated

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ curl -o input.md http://muistio.tieke.fi/ep/pad/export/russia-regional-inequalit
 
 #pandoc -s -S --number-section --toc --from=markdown+yaml_metadata_block -H /home/aurelius/web/css/rmarkdown.css input.md -o article_demokr.html
 # pdf
-pandoc --toc --number-section --latex-engine=xelatex -V lang=english -V papersize:a4paper -V documentclass=scrartcl input.md -o regional_inequality.pdf
+pandoc --number-section --latex-engine=xelatex -V lang=english -V papersize:a4paper -V documentclass=scrartcl input.md -o regional_inequality.pdf
 # word
 pandoc --toc --number-section input.md -o regional_inequality.docx
 pandoc -s -S -H /home/aurelius/web/css/rmarkdown.css index.md -o index.html
